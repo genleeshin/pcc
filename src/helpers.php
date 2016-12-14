239d5ac382d50fa20eef65ddd44dc5c324bca429
genleeshin <genl.lee.shin@gmail.com>
2016-12-14 21:42:01 +0000

    update helper added checking if func exists

--- src/helpers.php
+++ src/helpers.php
@@ -1,81 +1,113 @@
 <?php 
 
-function d($var){
-	if(is_array($var) || is_object($var))
-		return parr($var);
-		
-	var_dump($var);
-}
-
-function dd($var){
-	die(d($var));
-}
-
-function parr($array){
-	echo '<pre>' . var_dump($array) . '</pre>';
-}
-
-function cout($m){
-	echo "\n-------------------------";
-    echo $m;
-    echo "\n-------------------------";
-}
-
-function value($value, $default=''){
-	return isset($value)?$value:$default;
-}
-
-function config($key=false){
-    return array_get(PCC\Core\Config::all(), $key);
-
-}
-
-function array_get($array, $key, $default = null)
-{
-    if (is_null($key)) return $array;
-    if (isset($array[$key])) return $array[$key];
-
-    if(strpos($key, '.')===false) return [];
-    foreach (explode('.', $key) as $segment)
-    {
-        if ( ! is_array($array) || ! array_key_exists($segment, $array))
-        {
-            return $default;
-        }
-        $array = $array[$segment];
+if (! function_exists('d')) {
+    function d($var){
+    	if(is_array($var) || is_object($var))
+    		return parr($var);
+    		
+    	var_dump($var);
     }
-    return $array;
 }
 
-function output($tpl, $data=null)
-{
-    if($data)
-        extract($data);
+if (! function_exists('dd')) {
 
-    ob_start();
-
-    include($tpl);
-
-    $output = ob_get_contents();
-
-    ob_end_clean();
-
-    return $output; 
+    function dd($var){
+    	die(d($var));
+    }
 }
 
-function app($key, $value=null){
-    if($value) 
-        return PCC\Core\App::$key($value);
+if (! function_exists('parr')) {
 
-    return PCC\Core\App::$key();
+    function parr($array){
+    	echo '<pre>' . var_dump($array) . '</pre>';
+    }
 }
 
-function view($tpl, $data=null){
-    $tpl = app('root') . '/views/' . str_replace('.', '/', $tpl) . '.php';
+if (! function_exists('cout')) {
 
-    return output($tpl, $data);
+    function cout($m){
+    	echo "\n-------------------------";
+        echo $m;
+        echo "\n-------------------------";
+    }
 }
 
-function is_closure($t) {
-    return ($t instanceof \Closure);
+if (! function_exists('value')) {
+
+    function value($value, $default=''){
+    	return isset($value)?$value:$default;
+    }
+}
+
+if (! function_exists('config')) {
+
+    function config($key=false){
+        return array_get(PCC\Core\Config::all(), $key);
+
+    }
+}
+
+if (! function_exists('array_get')) {
+
+    function array_get($array, $key, $default = null)
+    {
+        if (is_null($key)) return $array;
+        if (isset($array[$key])) return $array[$key];
+
+        if(strpos($key, '.')===false) return [];
+        foreach (explode('.', $key) as $segment)
+        {
+            if ( ! is_array($array) || ! array_key_exists($segment, $array))
+            {
+                return $default;
+            }
+            $array = $array[$segment];
+        }
+        return $array;
+    }
+}
+
+if (! function_exists('output')) {
+
+    function output($tpl, $data=null)
+    {
+        if($data)
+            extract($data);
+
+        ob_start();
+
+        include($tpl);
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output; 
+    }
+}
+
+if (! function_exists('app')) {
+
+    function app($key, $value=null){
+        if($value) 
+            return PCC\Core\App::$key($value);
+
+        return PCC\Core\App::$key();
+    }
+}
+
+if (! function_exists('view')) {
+
+    function view($tpl, $data=null){
+        $tpl = app('root') . '/views/' . str_replace('.', '/', $tpl) . '.php';
+
+        return output($tpl, $data);
+    }
+}
+
+if (! function_exists('is_closure')) {
+
+    function is_closure($t) {
+        return ($t instanceof \Closure);
+    }
 }
